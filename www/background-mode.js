53c505ccb4c673b4c3ab486c609fce667ef72d47
Jean Marc BRUNO <technique@edps.fr>
2014-12-13 03:15:41 +0100

    modif texte notification

--- www/background-mode.js
+++ www/background-mode.js
@@ -44,9 +44,9 @@ channel.deviceready.subscribe(function () {
  */
 exports.getDefaults = function () {
     return {
-        title:  'App is running in background',
-        text:   'Doing heavy tasks.',
-        ticker: 'App is running in background',
+        title:  'Assperia',
+        text:   'App est en arrière plan',
+        ticker: 'Assperia',
         resume: true
     };
 };
